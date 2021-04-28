981000c1a6f5dadbd90061b263168a75ed73d908
amiraatalla <amiraatalla63@gmail.com>
2021-04-28 05:08:54 +0200

    todos-api-auth

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
 const EmployeeRoute = require('./routes/Employee')
+const AuthRoute = require('./routes/auth')
+
 mongoose.connect('mongodb://localhost:27017/testdb', { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
@@ -20,11 +22,13 @@ const app = express()
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use('/uploads', express.static('./uploads')); //to get image
 
 const PORT = process.env.PORT || 3000
-
+ 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
 
 app.use('/api/employee',EmployeeRoute)
+app.use('/api',AuthRoute)
